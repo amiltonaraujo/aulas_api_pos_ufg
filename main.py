c26f9615075aae5b017638138c7584bf6e613655
Amilton Araujo <amilton@amilton.eti.br>
2026-05-16 12:12:39 -0300

    Versão inicial do fastapi

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/teste")
+def hello_world():
+     return {"mensagem": "Teste API - Hello World"}
+
+@app.get(path="/soma/{numero1}/{numero2}")
+def soma(numero1: int, numero2: int):
+    total = numero1 + numero2
+    return {"resultado":total}
