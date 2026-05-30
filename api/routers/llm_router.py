ffb261ecf4eba8ca8e76dc62082922a64e637e7c
Amilton Araujo <amilton@amilton.eti.br>
2026-05-30 10:45:54 -0300

    updates

--- api/routers/llm_router.py
+++ api/routers/llm_router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
-
 from models import Historia
 from utils import execute_prompt, get_logger
-
 
 logger = get_logger()
 router = APIRouter()
@@ -11,9 +9,7 @@ router = APIRouter()
 @router.post("/gerar_historia/v1")
 def gerar_historia(historia: Historia):
     logger.info(f"Recebido pedido para gerar história com tema: {historia.tema}")
-
     prompt = f"Escreva uma história sobre o tema: {historia.tema}"
-
     resposta = execute_prompt(prompt)
     logger.info(f"História gerada com sucesso para o tema: {historia.tema}")
 
